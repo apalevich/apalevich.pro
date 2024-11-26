78d3732a1c85dbf9b86118215aeecf87966d1e41
Artyom Palevich <backstageofme@gmail.com>
2024-11-26 14:27:53 +0700

    Draft mobile version

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,9 +2,23 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			'pt-serif': ['PT Serif', 'serif'],
+			'montserrat': ['Montserrat', 'sans-serif']
+		},
 		extend: {
 			colors: {
-				secondary: 'rgb(244, 242, 237)',
+				// secondary: 'rgb(244, 242, 237)',
+				
+				black: 'rgb(99, 102, 241)',
+				indigo: {
+					// 50: 'white',
+				},
+			},
+			backgroundImage: {
+				'underline1': "url('/Underline1.svg')",
+				'underline2': "url('/Underline2.svg')",
+				'underline3': "url('/Underline3.svg')",
 			},
 		},
 	},
