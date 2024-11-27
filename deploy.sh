81f7dcd475482409793c125a3805629107fba388
Artyom Palevich <backstageofme@gmail.com>
2024-11-27 19:06:02 +0700

    Prepare deploy automation

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+PROJECT_DIR="/var/www/apalevich.pro"
+LOG_FILE="/var/www/apalevich.pro/logs"
+
+echo "---- Deployment started: $(date) ----" >> $LOG_FILE
+
+cd $PROJECT_DIR || exit
+git reset --hard >> $LOG_FILE 2>&1
+git pull origin main >> $LOG_FILE 2>&1
+npm ci >> $LOG_FILE 2>&1
+npm run build >> $LOG_FILE 2>&1
+
+echo "---- Deployment finished: $(date) ----" >> $LOG_FILE
