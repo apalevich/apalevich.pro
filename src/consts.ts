99ba5cbee74b0936b2e9d7b7597ceb44786e6c23
Artyom Palevich <backstageofme@gmail.com>
2024-11-28 18:23:34 +0700

    Minor updates

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'A.Palevich\'s Front-End Bureau';
+export const SITE_TITLE = 'Front-End Bureau A.Palevich\' Pro';
 export const SITE_DESCRIPTION = 'We deliver clear and measurable projects where quality is more important than hours.';
