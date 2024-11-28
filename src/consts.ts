67bad05b0cf21c9bccb3f9b1ea5453dc222bbe9d
Artyom Palevich <backstageofme@gmail.com>
2024-11-28 17:38:47 +0700

    Update constant title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'A.Palevich\'s Front-End Bureau';
+export const SITE_DESCRIPTION = 'We deliver clear and measurable projects where quality is more important than hours.';
