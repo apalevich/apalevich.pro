9e0b3dc684b92aa5f4e2f1bb53dd414c8d9fe49f
Artyom Palevich <backstageofme@gmail.com>
2024-12-02 13:02:08 +0700

    Enhance Tailwind configuration and update SVG assets

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,25 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: false, // or 'media' or 'class',
 	theme: {
+		colors: {
+			secondary: "#white",
+			black: "rgb(99, 102, 241)",
+			white: "white",
+		},
 		fontFamily: {
-			'pt-serif': ['PT Serif', 'serif'],
-			// 'montserrat': ['Montserrat', 'sans-serif']
+			"pt-serif": ["PT Serif", "serif"],
+			montserrat: ["Montserrat", "sans-serif"],
+		},
+		backgroundSize: {
+			auto: "auto",
+			cover: "cover",
+			contain: "contain",
+			"100%": "100%",
 		},
 		extend: {
-			colors: {
-				// secondary: 'rgb(244, 242, 237)',
-				
-				black: 'rgb(99, 102, 241)',
-				indigo: {
-					// 50: 'white',
+			backgroundImage: {
+				'underline1': "url('/src/images/Underline1.svg')",
+				'underline1-flip': "url('/src/images/Underline1-flip.svg')",
+				'underline2': "url('/src/images/Underline2.svg')",
+				'underline3': "url('/src/images/Underline3.svg')",
+			},
+			keyframes: {
+				"fade-in-down": {
+				  	"0%": {
+						opacity: "0",
+						transform: "translateY(-10px)",
+				  	},
+				  	"100%": {
+						opacity: "1",
+						transform: "translateY(0)",
+					},
 				},
 			},
-			backgroundImage: {
-				'underline1': "url('/Underline1.svg')",
-				'underline1-flip': "url('/Underline1-flip.svg')",
-				'underline2': "url('/Underline2.svg')",
-				'underline3': "url('/Underline3.svg')",
+			  animation: {
+				"fade-in-down": "fade-in-down 0.5s ease-out",
 			},
 		},
 	},
