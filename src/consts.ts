732ca16c80bcdc6e1d57cf8dd5b550e80d74f354
Artyom Palevich <backstageofme@gmail.com>
2024-12-09 10:16:11 +0700

    Prettier write

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Front-End Bureau A.Palevich\' Pro';
-export const SITE_DESCRIPTION = 'We deliver clear and measurable projects where quality is more important than hours.';
+export const SITE_TITLE = "Front-End Bureau A.Palevich' Pro";
+export const SITE_DESCRIPTION =
+	"We deliver clear and measurable projects where quality is more important than hours.";
