1cc48c1f8b28ae3a11aaa085c7dceb0f3cbc814d
Artyom Palevich <backstageofme@gmail.com>
2024-12-24 12:49:41 +0700

    Move PageCopyInterface and ListItem to a new file and clean up textcopy.ts

---
+++ src/interfaces/index.ts
@@ -0,0 +1,52 @@
+interface ListItem {
+    icon?: number|string,
+    title: string;
+    description: string;
+}
+
+interface PageCopyInterface {
+    hero?: {
+        h1: string;
+        h2: string;
+        buttons: {
+            text: string;
+            url: string|null;
+            primary: boolean;
+        }[];
+    };
+    services?: {
+        title: string;
+        services: ListItem[];
+    };
+    staff?: {
+        title: string;
+        cards?: ListItem[];
+    },
+    flow?: {
+        title: string;
+        steps: ListItem[];
+    },
+    banner?: {
+        title: string;
+        description: string;
+        button: {
+            text: string;
+            url?: string;
+        },
+        image?: string;
+    },
+    proof?: {
+        title: string;
+        testimonials: {
+            quote: string;
+            title: string;
+            role: string;
+            company: string;
+        }[];
+    }
+}
+
+export type {
+    ListItem,
+    PageCopyInterface
+}
