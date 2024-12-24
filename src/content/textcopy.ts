31f93b42c8e24598d46690010ef5bc96e3105e4e
Artyom Palevich <backstageofme@gmail.com>
2024-12-24 12:43:53 +0700

    Add page copy with structured content

---
+++ src/content/textcopy.ts
@@ -0,0 +1,258 @@
+let agencyPageCopy: PageCopyInterface;
+let marcomPageCopy: PageCopyInterface;
+
+interface ListItem {
+    icon?: number|string,
+    title: string;
+    description: string;
+}
+
+interface PageCopyInterface {
+    hero?: {
+        h1: string;
+        h2: string;
+        buttons: {
+            text: string;
+            url: string|null;
+            primary: boolean;
+        }[];
+    };
+    services?: {
+        title: string;
+        services: ListItem[];
+    };
+    staff?: {
+        title: string;
+        cards?: ListItem[];
+    },
+    flow?: {
+        title: string;
+        steps: ListItem[];
+    },
+    banner?: {
+        title: string;
+        description: string;
+        button: {
+            text: string;
+            url?: string;
+        },
+        image?: string;
+    },
+    proof?: {
+        title: string;
+        testimonials: {
+            quote: string;
+            title: string;
+            role: string;
+            company: string;
+        }[];
+    }
+}
+
+agencyPageCopy = {
+    hero: {
+        h1: "Web development with results at the forefront",
+        h2: "We are a web development agency that delivers clear and measurable projects where quality is more important than hours through our Front‑End expertise",
+        buttons: [
+            {
+                text: "Let’s Talk",
+                primary: true,
+                url: null,
+            },
+            {
+                text: "View Study Cases",
+                primary: false,
+                url: null,
+            },
+        ]
+    },
+    services: {
+        title: "What we de",
+        services: [
+            {
+                icon: "1",
+                title: "Early-Stage Web Development",
+                description: "Every decision made in the early stages impacts the future of your product — trust our experts to get it right from the start."
+            },
+            {
+                icon: "2",
+                title: "Supporting Creative Teams",
+                description: "With extensive experience working alongside designers and marketers, we ensure the smooth integration of technical solutions into your campaigns and strategies."
+            },
+            {
+                icon: "3",
+                title: "Auditing and Consulting",
+                description: "With experience in renowned international companies, we provide top-tier audits and actionable insights."
+            },
+        ],
+    },
+    staff: {
+        title: "Our team is made up exclusively of senior professionals with up to 8 years of proven track record of success in their respective fields.",
+        cards: [
+            {
+                icon: null,
+                title: "Zarema Khalilova",
+                description: "Front-end Lead Engineer with extensive expertise in Search Engine Optimization (SEO)",
+            },
+            {
+                icon: null,
+                title: "OLGA KOBCHENKO",
+                description: "Agile Coach and Counsellor with expertise in optimizing teams workflow and productivity",
+            },
+            {
+                icon: null,
+                title: "DARIA PONOMAREVA",
+                description: "Experienced UX Research Lead with a focus on product culture integration",
+            },
+            {
+                icon: null,
+                title: "EUGENE KHOMUTENKO",
+                description: "Design manager with four years of experience leading cross-platform design teams at international fintech companies",
+            },
+        ]
+    },
+    flow: {
+        title: "How It Works",
+        steps: [
+            {
+                title: "Exploration",
+                description: "We start with a few emails and a call. During the call, we dive deep into your business conditions, priorities, and specific project requirements.,"
+            },
+            {
+                title: "Specialist Scouting",
+                description: "We scout top-tier specialists with the relevant experience and skills for your project.,"
+            },
+            {
+                title: "Defining the Project",
+                description: "Together, we define the project scope, goals, stages, and expected outcomes. We determine the amount of effort required, necessary resources, specialist roles, and any access needed to your existing systems.,"
+            },
+            {
+                title: "Proposal and Agreement",
+                description: "With preliminary commitments from our specialists, we prepare a comprehensive commercial proposal for you. You review the proposal, and upon acceptance, we move forward.,"
+            },
+            {
+                title: "Team Assembly and Kickoff",
+                description: "A Project Manager is assigned as your primary point of contact and the liaison with the specialists.,"
+            },
+            {
+                title: "Completion & Feedback",
+                description: "We present the final results, meeting all predefined goals and criteria. Post-project, we exchange feedback to improve future collaborations and celebrate successes.,"
+            },
+        ]
+    },
+    proof: {
+        title: "Testimonials",
+        testimonials: [
+            {
+                quote: "Artem is an experienced developer. I was really happy to work with him!",
+                title: "Kirill Zhirnov",
+                role: "CEO",
+                company: "Boundless Commerce"
+            }
+        ]
+    }
+}
+
+marcomPageCopy = {
+    hero: {
+        h1: "Web Development services for your marketing success",
+        h2: "We manage the tech. You master the message.",
+        buttons: [
+            {
+                text: "Let’s Talk",
+                primary: true,
+                url: null,
+            },
+            {
+                text: "View Study Cases",
+                primary: false,
+                url: null,
+            },
+        ]
+    },
+    services: {
+        title: "What we de",
+        services: [
+            {
+                icon: "🎯",
+                title: "Website Maintenance Services",
+                description: "Seamless website support that keeps your core IT team focused on product development. We handle all technical updates, ensuring your online presence remains smooth and efficient."
+            },
+            {
+                icon: "📩",
+                title: "Email Marketing",
+                description: "Comprehensive email marketing solutions backed by expert strategic insights. We design, implement, and optimize campaigns that drive engagement and conversion."
+            },
+            {
+                icon: "👩🏻‍🔬",
+                title: "Hypothesis Validation",
+                description: "Rapid MVP development to test and validate your business ideas quickly. We back up you by setting up robust analytics systems to gather reliable, actionable data that guide your strategic decisions."
+            },
+        ],
+    },
+    staff: {
+        title: "With 8 years of experience supporting design and marketing departments, I have developed strong skills and built a solid network of top-tier specialists ready to collaborate:",
+        cards: [
+            {
+                icon: null,
+                title: "Zarema Khalilova",
+                description: "Front-end Lead Engineer with extensive expertise in Search Engine Optimization (SEO)",
+            },
+            {
+                icon: null,
+                title: "OLGA KOBCHENKO",
+                description: "Agile Coach and Counsellor with expertise in optimizing teams workflow and productivity",
+            },
+            {
+                icon: null,
+                title: "DARIA PONOMAREVA",
+                description: "Experienced UX Research Lead with a focus on product culture integration",
+            },
+            {
+                icon: null,
+                title: "EUGENE KHOMUTENKO",
+                description: "Design manager with four years of experience leading cross-platform design teams at international fintech companies",
+            },
+        ]
+    },
+    flow: {
+        title: "How It Works",
+        steps: [
+            {
+                title: "Exploration",
+                description: "We start with a few emails and a call. During the call, we dive deep into your business conditions, priorities, and specific project requirements.,"
+            },
+            {
+                title: "Specialist Scouting",
+                description: "We scout top-tier specialists with the relevant experience and skills for your project.,"
+            },
+            {
+                title: "Defining the Project",
+                description: "Together, we define the project scope, goals, stages, and expected outcomes. We determine the amount of effort required, necessary resources, specialist roles, and any access needed to your existing systems.,"
+            },
+            {
+                title: "Proposal and Agreement",
+                description: "With preliminary commitments from our specialists, we prepare a comprehensive commercial proposal for you. You review the proposal, and upon acceptance, we move forward.,"
+            },
+            {
+                title: "Team Assembly and Kickoff",
+                description: "A Project Manager is assigned as your primary point of contact and the liaison with the specialists.,"
+            },
+            {
+                title: "Completion & Feedback",
+                description: "We present the final results, meeting all predefined goals and criteria. Post-project, we exchange feedback to improve future collaborations and celebrate successes.,"
+            },
+        ]
+    },
+    proof: {
+        title: "Testimonials",
+        testimonials: [
+            {
+                quote: "Artem is an experienced developer. I was really happy to work with him!",
+                title: "Kirill Zhirnov",
+                role: "CEO",
+                company: "Boundless Commerce"
+            }
+        ]
+    }
+}
