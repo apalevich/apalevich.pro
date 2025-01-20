139eeb875a11d84baee6e61903c55d1ba3be216f
Artyom Palevich <backstageofme@gmail.com>
2025-01-20 11:47:53 +0700

    Moved new design files into the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
-// @ts-check
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-
-import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://apalevich.pro",
-	integrations: [mdx(), sitemap(), tailwind()],
-	experimental: {
-		svg: true,
-	},
+  site: "https://astroship.web3templates.com",
+  integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Front-End Bureau A.Palevich' Pro";
+export const SITE_TITLE = "APalevich.Pro";
 export const SITE_DESCRIPTION =
-	"We deliver clear and measurable projects where quality is more important than hours.";
+	"APalevich.Pro — Technical Solutions for Marketing Leaders";
