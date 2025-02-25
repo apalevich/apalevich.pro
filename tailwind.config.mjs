eaaf510c3085aaa7d9bd43d287d85a422fccda0c
Artyom Palevich <backstageofme@gmail.com>
2025-02-25 15:49:20 +0700

    Add client testimonials section with dynamic content and styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      // emerald: colors.emerald,
+      // indigo: colors.indigo,
+      // yellow: colors.yellow,
+    },
     extend: {
       fontFamily: {
         sans: [
@@ -40,7 +52,7 @@ module.exports = {
           800: "#031B1C",
           900: "#010E0E",
           950: "#010909"
-        }
+        },
       }
     },
   },
